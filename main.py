2a74d4c977ff57b2c730d0ec10fa26433088503f
meow <114763852+eofieoakfvoa@users.noreply.github.com>
2025-03-24 06:20:28 +0100

    :cat:

---
+++ main.py
@@ -0,0 +1,6 @@
+# https://www.w3schools.com/python/python_classes.asp
+class Item:
+    def __init__(self, name, pricesek, momspercent):
+        self.Name = name
+        self.priceSek = pricesek
+        self.MomsPercent = momspercent
