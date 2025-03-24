d9b238ec5d3e885a4ab29ae61687a2f6bb873d9a
meow <114763852+eofieoakfvoa@users.noreply.github.com>
2025-03-24 23:56:10 +0100

    zzzzzzzzzz

---
+++ Del1.py
@@ -0,0 +1,43 @@
+def ChooseAlternatives():
+    Price = "" #detta ser jätte skumt ut men fungerar :p
+    momsPercent = "" 
+    print("Hej snälla skriv först ett pris")
+
+    while not _isPriceValid(Price):
+        Price = input()
+        try:
+            Price = float(Price)
+        except:
+            print("Priset : ", Price, " är antingen inte ett nummer eller så innehåler det ett mellanslag")
+    
+    print("skriv nu moms I percent alltså t.ex '25' för 25%")
+    while not _isMomsValid(momsPercent):
+        momsPercent = input()
+        try:
+            momsPercent = float(momsPercent)
+        except:
+            print("MomsProcent på  : ", momsPercent, " är antingen inte ett nummer eller så innehåler det ett mellanslag")
+
+    appliedMomsPrice = _ApplyMoms(Price, momsPercent)
+
+    print("Priset :", Price, " med applicerad moms av ", momsPercent, "% Är lika med : ", round(appliedMomsPrice, 2))
+
+    input()
+
+def _ApplyMoms(price : float, moms : float):
+    newMoms = _convertMomsToDecimal(moms)
+    appliedMomsPrice = price * (1 + newMoms)
+    return appliedMomsPrice 
+
+def _isPriceValid(price):
+    if type(price) != float:
+        return False
+    return True #ksk lägga till så price inte kan vara negativt?
+    
+def _isMomsValid(moms):
+    if type(moms) != float:    
+        return False
+    return True
+
+def _convertMomsToDecimal(moms):
+    return moms * 0.01
--- main.py
+++ main.py
@@ -1,44 +1,22 @@
-def ChooseAlternatives():
-    Price = "" #detta ser jätte skumt ut???
-    momsPercent = "" 
-    print("Hej snälla skriv först ett pris")
-    while not isPriceValid(Price):
-        try:
-            Price = float(input())
-        except:
-            continue
-    
-    print("skriv nu moms I percent alltså t.ex '25'")
-    while not isMomsValid(momsPercent):
-        try:
-            momsPercent = float(input())
-        except:
-            continue
-
-    print(Price, momsPercent)
-    appliedMomsPrice = ApplyMoms(Price, momsPercent)
-    print("Priset :", Price, " med applicerad moms av ", momsPercent, "% Är lika med : ", appliedMomsPrice)
-
-def ApplyMoms(price : float, moms : float):
-    if not isPriceValid(price):
-        print("aa ",type(price))
-        return 
-    if not isMomsValid(moms):
-        
-        return
-    newMoms = convertMomsToDecimal(moms)
-    appliedMomsPrice = price * (1 + newMoms)
-    return appliedMomsPrice 
-
-def isPriceValid(price):
-    if type(price) != float:
-        return False
-    return True #ksk lägga till så price inte kan vara negativt?
-    
-def isMomsValid(moms):
-    if type(moms) != float or int:    
-        return False
-    return True
-def convertMomsToDecimal(moms):
-    return moms * 0.01
-ChooseAlternatives()
+import Del1
+import Del2
+import Del3
+def main():
+    print(
+        "Skriv en av de följande \n"
+        "[1] = Momskalkylator \n"
+        "[2] = Kolla hur många stora och små bokstäver I en text \n"
+        "[3] = Kolla Ifall ett ord är en palindrom "
+        )
+    Alternative = input()
+    #Det hade nog varit smartare att sätta alternativen i en Dictionary sen göra Dictionary[alternativ].Start() som skulle kunna runna coden men jag lowkey pallar inte
+    #Där Dictionary innehåller en index som går till modulen och callar start, då alla filer förväntar innehålla en start() function
+    #och eftersom jag vet att jag inte behöver expandera projectet mer än 3 så borde det vara okej att hard coda in detta och att uppgiften är på E nivå :P
+    if Alternative == "1": 
+        from Del1 import ChooseAlternatives
+        ChooseAlternatives()
+    if Alternative == "2":
+        pass
+    if Alternative == "3":
+        pass
+main()
