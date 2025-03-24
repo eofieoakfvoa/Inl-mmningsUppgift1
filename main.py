f21245da4f78b1b34ae08736e8ed632b6da84cca
meow <114763852+eofieoakfvoa@users.noreply.github.com>
2025-03-24 21:57:11 +0100

    bbbbbb

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 def ChooseAlternatives():
-    print("Hej snälla skriv ett pris och dess moms I percent alltså t.ex '0.25' ")
-    ApplyMoms()
+    print("Hej snälla skriv först ett pris")
+    Price = input()
+    print("moms I percent alltså t.ex '25'")
+    momsPercent = input()
+    print(Price, momsPercent)
+    appliedMomsPrice = ApplyMoms(Price, momsPercent)
     print("Priset :", "", " med applicerad moms av ", "", " Är lika med : ", "")
-def ApplyMoms(price, moms):
+def ApplyMoms(price : float, moms : float):
     if not isPriceValid(price):
-        pass
+        print("aa ",type(price))
+        return 
     if not isMomsValid(moms):
-        pass
+        
+        return
     newMoms = convertMomsToDecimal(moms)
+    appliedMomsPrice = price * (1 + newMoms)
     return price, moms, appliedMomsPrice 
 
 def isPriceValid(price):
-    pass
+    if type(price) != float or int:
+        print("The price of : ", price, " is not a valid number :(")
+        return False
+    return True #ksk lägga till så price inte kan vara negativt?
+    
 def isMomsValid(moms):
-    pass
+    if type(moms) != float or int:    
+        return False
+    return True
 def convertMomsToDecimal(moms):
-    pass
+    return moms * 0.01
+ChooseAlternatives()
